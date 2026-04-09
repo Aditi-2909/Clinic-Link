a2bc761e6123a4e9c6f2f1a3c8a475bc96b6f12e
Aditi Das <replyaditi01@gmail.com>
2026-04-10 02:11:38 +0530

    schema done

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("DB error:", error.message);
+  }
+};
+
+export default connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express'  // node.js deye server bananor famework[login,appoinment,doctorlist etc]
 import cors from 'cors'   // cors forntend theke backend call korte deye
 import 'dotenv/config'  // .env file theke secret data neye
+import  connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
 
 //app config ... app ar basic set up
 const app = express()
 const port = process.env.PORT || 4000
-
+connectDB()
+connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors())

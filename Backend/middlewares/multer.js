75140a96643164ce3befa8114e249bd6306cbd0d
Aditi Das <replyaditi01@gmail.com>
2026-04-11 13:18:59 +0530

    update

--- Backend/middlewares/multer.js
+++ Backend/middlewares/multer.js
@@ -1,12 +1,35 @@
-import multer from 'multer'
+// import multer from 'multer';
+
+// // Set up multer storage configuration
+// const storage = multer.diskStorage({
+//     filename: function(req,file,callback){
+//         callback(null, file.originalname)
+//     }
+// })
+
+// const upload = multer({storage})
+
+// export default upload;
 
 
+
+
+
+
+import multer from 'multer';
+import path from 'path';
+
+// storage config
 const storage = multer.diskStorage({
-    filename: function (req,file,callback) {
-        callback(null,file.originalname)
+    destination: function (req, file, cb) {
+        cb(null, 'upload/'); // folder create kor (important)
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = Date.now() + path.extname(file.originalname);
+        cb(null, uniqueName);
     }
-})
+});
 
-const uplod = multer({storage})
+const upload = multer({ storage });
 
-export default uplod
+export default upload;
--- Backend/server.js
+++ Backend/server.js
@@ -1,25 +1,28 @@
-import express from 'express'  // node.js deye server bananor famework[login,appoinment,doctorlist etc]
-import cors from 'cors'   // cors forntend theke backend call korte deye
-import 'dotenv/config'  // .env file theke secret data neye
-import  connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
-//app config ... app ar basic set up
-const app = express()
-const port = process.env.PORT || 4000
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
 connectDB()
 connectCloudinary()
+
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors());
 
 // api endpoints
-app.use('/api/admin',adminRouter)
-// localhost:4000/api/admin/add-doctor
+app.use('/api/admin', adminRouter);
+//localhost:4000/api/admin/add-doctor
 
-app.get('/',(req,res)=>{
-    res.send('API WORKING hello')
+app.get('/', (req,res)=>{
+    res.send('API WORKING');
 })
 
-app.listen(port, ()=>console.log("Server Start ",port))
+app.listen(port, ()=>{
+    console.log("Server started", port);
+});

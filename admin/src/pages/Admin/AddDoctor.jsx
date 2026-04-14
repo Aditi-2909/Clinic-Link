a399fb849beb9f268847eb29ff3e2bcfdd068144
Aditi Das <replyaditi01@gmail.com>
2026-04-15 02:12:21 +0530

    doctors-list done

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -51,15 +51,24 @@ const AddDoctor = () => {
       const {data} = await axios.post(backendUrl + '/api/admin/add-doctor', formData, {headers:{aToken, 'Content-Type':'multipart/form-data'}})
       if (data.success) {
         toast.success(data.message)
+        setDocImg(false)
+        setName('')
+        setPassword('')
+        setEmail('')
+        setAddress1('')
+        setAddress2('')
+        setDegree('')
+        setAbout('')
+        setFees('')
       } else {
         toast.error(data.message)
       }
       
 
 
-    } catch (error) {
-  //     console.log(error)
-  // toast.error(error.message)
+    } catch (error) {   
+  toast.error(error.message)
+  console.log(error)
     }
   }
 

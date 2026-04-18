4d862f3657a830e15c1fbac8d815465d0bc53ee0
Aditi Das <replyaditi01@gmail.com>
2026-04-19 00:52:14 +0530

    get profile user testing just done in postman

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,73 +3,88 @@ import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 //Api to register user
-const registerUser = async (req,res) =>{
+const registerUser = async (req, res) => {
 
-    try{
-      
-        const {name,email,password}=req.body
+    try {
 
-        if(!name || !password || !email){
-            return res.json({success:false,message:"Missing Details"})
+        const { name, email, password } = req.body
+
+        if (!name || !password || !email) {
+            return res.json({ success: false, message: "Missing Details" })
         }
-       // validating email format
-        if(!validator.isEmail(email)){
-            return res.json({success:false,message:"enter a valid email"})
+        // validating email format
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "enter a valid email" })
         }
         //validating strong password
-        if(password.length < 8){
-            return res.json({success:false,message:"enter a Strong password"})
+        if (password.length < 8) {
+            return res.json({ success: false, message: "enter a Strong password" })
         }
 
         //hashing user password
 
         const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
-        
+        const hashedPassword = await bcrypt.hash(password, salt)
+
         const userData = {
             name,
             email,
-            password:hashedPassword
+            password: hashedPassword
         }
 
         const newUser = new userModel(userData)
         const user = await newUser.save()
-          
-
-        const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
-        res.json({success:true,token})
 
 
-    }catch(error){
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        res.json({ success: true, token })
+
+
+    } catch (error) {
         console.log(error)
-        res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
     }
-    
+
 }
 
 //API for user login
-const loginUser = async(req,res)=>{
-    try{
+const loginUser = async (req, res) => {
+    try {
 
-        const {email,password} =req.body
-        const user = await userModel.findOne({email})
-        
-        if(!user){
-           return res.json({success:false,message:"user does not exist"})
+        const { email, password } = req.body
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.json({ success: false, message: "user does not exist" })
         }
 
-        const isMatch = await bcrypt.compare(password,user.password)
+        const isMatch = await bcrypt.compare(password, user.password)
 
-        if(isMatch){
-            const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
-            res.json({success:true,token})
+        if (isMatch) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            res.json({ success: true, token })
 
-        }else{
-            res.json({success:false,message:"Invalid credentials"})
+        } else {
+            res.json({ success: false, message: "Invalid credentials" })
         }
-    }catch(error){
+    } catch (error) {
         console.log(error)
-        res.json({success:false,message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
-export{registerUser,loginUser}
+
+//api to get user profile data
+const getProfile = async (req, res) => {
+    try {
+
+        // const { userId } = req.body
+        const userId  = req.userId
+        const userData = await userModel.findById(userId).select('-password')
+
+        res.json({ success: true, userData })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+export { registerUser, loginUser, getProfile }

4c053c6489555180787171729d06a92b0767cce2
Aditi Das <replyaditi01@gmail.com>
2026-04-22 22:00:06 +0530

    just coading

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -180,5 +180,20 @@ try{
         res.json({ success: false, message: error.message })
     }
 
+
 }
-export { registerUser, loginUser, getProfile ,updateProfile,bookAppointment}
+// api to get user appointments for frontend my -appointments page
+    const listAppointment = async (req,res)=>{
+        try{
+
+            const userId = req.userId
+            const appointments = await appointmentModel.find({userId})
+
+            res.json({success:true,appointments})
+
+        } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+export { registerUser, loginUser, getProfile ,updateProfile,bookAppointment,listAppointment}

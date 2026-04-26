d28c27979280c7a4058dd2214929711925281092
Aditi Das <replyaditi01@gmail.com>
2026-04-26 13:13:23 +0530

    dashbord complete

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors,setDoctors] = useState([])
     const[appointments,setAppointments] = useState([])
+    const [dashData,setDashData] = useState(false)
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async () => {
@@ -77,6 +78,23 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const getDashData = async ()=>{
+        try{
+         
+         const {data} = await axios.get(backendUrl +'/api/admin/dashboard',{headers:{aToken}})
+         if(data.success){
+            setDashData(data.dashData)
+            console.log(data.dashData);
+            
+         }else{}
+         toast.error(data.message)
+
+        }catch(error){
+          toast.error(error.message)
+          
+        }
+    }
+
     const value = {
         aToken,
         setAToken,
@@ -86,7 +104,8 @@ const AdminContextProvider = (props) => {
         changeAvailability,
         appointments,setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        dashData,getDashData
     }
 
     return (

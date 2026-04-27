0fa37347958403da1581fa171032ae14fb685282
Aditi Das <replyaditi01@gmail.com>
2026-04-27 19:14:00 +0530

    doctor dashboard done

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -4,6 +4,7 @@ import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 //import { useContext } from 'react'
 import { toast } from 'react-toastify'
+import { DoctorContext } from '../context/DoctorContext'
 const Login = () => {
 
     const [state,setState]=useState('Admin')
@@ -11,6 +12,7 @@ const Login = () => {
     const [password,setPassword]=useState('')
 
     const {setAToken,backendUrl} =useContext(AdminContext)
+    const {setDToken} = useContext(DoctorContext)
 
     const onSubmitHandeler = async(event)=>{
       event.preventDefault()
@@ -25,6 +27,20 @@ const Login = () => {
              else{
                toast.error(data.message)
              }
+          }else{
+
+            const {data} = await axios.post(backendUrl+'/api/doctor/login',{email,password})
+            if(data.success){
+               localStorage.setItem('dToken',data.token)
+                setDToken(data.token)
+                console.log(data.token);
+                
+             }
+             else{
+               toast.error(data.message)
+             }
+
+
           }
       }catch(error){
 
